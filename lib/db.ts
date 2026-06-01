4e0875c565817bc8db97d2956d385b3fc3887b8e
Hermann2710 <douanlahermann93@gmail.com>
2026-06-01 14:29:49 +0100

    Use bibliogest database for Mongo client

--- lib/db.ts
+++ lib/db.ts
@@ -6,6 +6,7 @@ if (!process.env.MONGODB_URI) {
 
 const uri = process.env.MONGODB_URI;
 const options = {
+  dbName: "bibliogest",
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
